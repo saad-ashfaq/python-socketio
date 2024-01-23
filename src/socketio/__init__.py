662d9b5dd83a2fb951098fd9d82bf86bf3786678
Saad Ashfaq <saadashfaq17@gmail.com>
2024-01-23 00:31:36 -0500

    Redis Sentinel Manager for SocketIO Pub/Sub
    - Added new SocketIO client manager called RedisSentinelManager as child
      class of RedisManager that extends Redis functionality with support
      for Sentinel setup.
    - Updated _redis_connect() to connect to current master from Sentinel
      with TCP keepalive option to detect disconnected server
    - Updated _publish() to always call _redis_connect() to get the current
      master from Sentinel before publishing
    - Updated _redis_listen_with_retries() to use non-blocking polling with
      sleep rather than blocking on pubsub.listen()
    - Added pubsub for Redis Sentinel on channel +switch-master to
      immediately detect failover scenarios and call _redis_connect() to
      get new master
    
    Overall, the RedisSentinelManager works well as long as the
    +switch-master message is received upon failover in the listen thread.
    However, connection timeout including TCP timeout when a server
    disconnects does not work reliably if the +switch-master message is
    missed. KombuManager with TCP keepalive seems to work reliably and will
    be used in production but keeping these changes in a separate branch
    for reference.
    
    Signed-off-by: Saad Ashfaq <saadashfaq17@gmail.com>

--- src/socketio/__init__.py
+++ src/socketio/__init__.py
@@ -5,6 +5,7 @@ from .base_manager import BaseManager
 from .pubsub_manager import PubSubManager
 from .kombu_manager import KombuManager
 from .redis_manager import RedisManager
+from .redis_manager import RedisSentinelManager
 from .kafka_manager import KafkaManager
 from .zmq_manager import ZmqManager
 from .server import Server
@@ -28,8 +29,9 @@ else:  # pragma: no cover
     AsyncAioPikaManager = None
 
 __all__ = ['Client', 'Server', 'BaseManager', 'PubSubManager',
-           'KombuManager', 'RedisManager', 'ZmqManager', 'KafkaManager',
-           'Namespace', 'ClientNamespace', 'WSGIApp', 'Middleware']
+           'KombuManager', 'RedisManager', 'RedisSentinelManager',
+           'ZmqManager', 'KafkaManager', 'Namespace', 'ClientNamespace',
+           'WSGIApp', 'Middleware']
 if AsyncServer is not None:  # pragma: no cover
     __all__ += ['AsyncClient', 'AsyncServer', 'AsyncNamespace',
                 'AsyncClientNamespace', 'AsyncManager', 'AsyncRedisManager',
--- src/socketio/redis_manager.py
+++ src/socketio/redis_manager.py
@@ -115,3 +115,83 @@ class RedisManager(PubSubManager):  # pragma: no cover
                     message['type'] == 'message' and 'data' in message:
                 yield message['data']
         self.pubsub.unsubscribe(self.channel)
+
+import socket
+class RedisSentinelManager(RedisManager):
+    name = 'sentinel'
+
+    def __init__(self, sentinels=[('localhost', 26379)], channel='socketio',
+                 master_name='mymaster', socket_timeout=0.1, db=0,
+                 password=None, write_only=False, logger=None):
+        if redis is None:
+            raise RuntimeError('Redis package is not installed '
+                               '(Run "pip install redis" in your '
+                               'virtualenv).')
+        self.sentinel = redis.Sentinel(sentinels, socket_timeout=socket_timeout)
+        self.failover = redis.Redis(host=sentinels[0][0], port=sentinels[0][1]).pubsub()
+        self.failover.psubscribe('+switch-master')
+        self.redis_options = {
+            'master_name': master_name,
+            'socket_timeout': socket_timeout,
+            'db': db,
+            'password': password
+        }
+        self._redis_connect()
+        super(RedisManager, self).__init__(channel=channel,
+                                           write_only=write_only,
+                                           logger=logger)
+
+    def _redis_connect(self):
+        master_ip, master_port = self.sentinel.discover_master(self.redis_options['master_name'])
+        self.redis_url = 'redis://:' + self.redis_options['password'] + '@' + master_ip + ':' + str(master_port) + '/' + str(self.redis_options['db'])
+        self.redis = redis.Redis.from_url(
+            self.redis_url,
+            socket_keepalive=True if hasattr(socket, 'TCP_KEEPIDLE') else None,
+            socket_keepalive_options={
+                socket.TCP_KEEPIDLE: 5,
+                socket.TCP_KEEPINTVL: 3,
+                socket.TCP_KEEPCNT: 3
+            } if hasattr(socket, 'TCP_KEEPIDLE') else None
+        )
+        self.pubsub = self.redis.pubsub(ignore_subscribe_messages=True)
+
+    def _publish(self, data):
+        retry = True
+        while True:
+            try:
+                self._redis_connect()
+                return self.redis.publish(self.channel, pickle.dumps(data))
+            except redis.exceptions.RedisError:
+                if retry:
+                    logger.error('Cannot publish to redis... retrying')
+                    retry = False
+                else:
+                    logger.error('Cannot publish to redis... giving up')
+                    break
+
+    def _redis_listen_with_retries(self):
+        retry_sleep = 1
+        connect = False
+        while True:
+            try:
+                if connect:
+                    self._redis_connect()
+                    self.pubsub.subscribe(self.channel)
+                    retry_sleep = 1
+                if self.failover.get_message(ignore_subscribe_messages=True):
+                    print("FAILOVER")
+                    self._redis_connect()
+                    self.pubsub.subscribe(self.channel)
+                message = self.pubsub.get_message(ignore_subscribe_messages=True)
+                if message:
+                    yield message
+                time.sleep(0.001)
+            except redis.exceptions.RedisError as e:
+                print(e)
+                logger.error('Cannot receive from redis... '
+                             'retrying in {} secs'.format(retry_sleep))
+                connect = True
+                time.sleep(retry_sleep)
+                retry_sleep *= 2
+                if retry_sleep > 60:
+                    retry_sleep = 60
